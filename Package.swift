e3162eaff91f47a95d61a0d37f2ec1ea983fdacc
Mike Green <chackle@gmail.com>
2024-03-06 11:48:22 +0000

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "CoinbaseWalletSDK",
-    platforms: [.iOS(.v15)],
+    platforms: [.iOS(.v13)],
     products: [
         .library(
             name: "CoinbaseWalletSDK",
